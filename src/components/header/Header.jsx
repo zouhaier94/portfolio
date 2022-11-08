acb144802b02a915e43adc6271462cbab0c632be
Zouhaier <uas0004398>
2022-11-08 18:51:46 +0100

    Update Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './header.css'
+import CTA from '../header/CTA'
+import ME from '../../images/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header--container">
+        <h5>Hello I'm</h5>
+        <h1>Zouhaier Boudaya</h1>
+        <h5 className="text-light">Front-End Developer</h5>
+        <CTA/>
+        <HeaderSocials/>
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll--down'> Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
