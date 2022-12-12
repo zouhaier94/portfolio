e6a88fc0f660a3cead3898b59bd92eb376166695
Zouhaier94 <zouhaier.boudaya94@gmail.com>
2022-12-12 22:51:47 +0100

    test

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import './header.css'
 import {BsLinkedin} from 'react-icons/bs'
 import {FaGithub} from 'react-icons/fa'
-import CV from '../../assets/CV.pdf'
+import CV from '../../assets/cv.pdf'
 
 const Header = () => {
   return (
